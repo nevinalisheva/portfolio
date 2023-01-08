aeec6a56dc5d13e5f7e448ee6e0ef195a58ae0df
Nevin Alisheva <nevin.alisheva@gmail.com>
2023-01-08 17:38:18 +0100

    tailwind animations

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import ContainerBlock from "../components/ContainerBlock";
+import About from "../sections/About";
 // import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
@@ -11,23 +12,49 @@ export default function Home() {
       title="Nevin Alisheva - Junior Full Stack Developer"
       description="Lorem Ipsum.. DESCRIPTION"
     >
-      <h1 className="text-3xl font-bold underline">Hi all, I am Nevin👋!</h1>
-      <p>
-        A passionate Full Stack Web Developer having an experience of building
-        Web and Mobile applications with JavaScript / Reactjs / Nodejs / Nextjs
-        and some other cool libraries and frameworks.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti error
-        eos ratione natus ut quas sed culpa, minus libero numquam eligendi
-        provident laudantium cumque quis ad maiores iusto praesentium explicabo.
-      </p>
+      <Experience />
+      <About />
+      <h3 class="text-3xl font-bold text-indigo-500 animate-bounce">
+        Tailwind CSS Animation
+      </h3>
+      {/* <h3 class="text-3xl font-bold text-indigo-500 animate-ping">
+        Tailwind CSS Animation
+      </h3> */}
+      {/* <div class="w-20 h-20 p-2 bg-blue-500 rounded-md animate-spin"></div>
+      <div class="w-20 h-20 p-2 bg-purple-500 rounded-md animate-ping"></div>
+      <div class="w-20 h-20 p-2 bg-green-500 rounded-md animate-bounce"></div>
+      <div class="w-20 h-20 p-2 bg-purple-500 rounded-md animate-pulse"></div> */}
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti error
         eos ratione natus ut quas sed culpa, minus libero numquam eligendi
         provident laudantium cumque quis ad maiores iusto praesentium explicabo.
       </p>
 
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti error
+        eos ratione natus ut quas sed culpa, minus libero numquam eligendi
+        provident laudantium cumque quis ad maiores iusto praesentium explicabo.
+      </p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="w-12 h-12 text-gray-600 animate-spin"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+        />
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti error
         eos ratione natus ut quas sed culpa, minus libero numquam eligendi
@@ -67,7 +94,6 @@ export default function Home() {
             </a>
           </div>
         </main> */}
-
       {/* <footer className={styles.footer}>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
---
+++ sections/About.js
@@ -0,0 +1,20 @@
+const About = () => {
+    return (
+      <div class="antialiased overflow-hidden">
+    <img  class="h-screen w-screen object-cover relative top-0 filter contrast-125" src="https://images.unsplash.com/photo-1564951434112-64d74cc2a2d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3387&q=80" alt="ss">
+
+    
+        <h1 className="text-3xl flex font-semibold text-purple-500">
+          Hi all, I am Nevin<span class="animate-waving-hand">👋🏻</span>!
+        </h1>
+        <p>
+          A passionate Full Stack Web Developer having an experience of building
+          Web and Mobile applications with JavaScript / Reactjs / Nodejs /
+          Nextjs and some other cool libraries and frameworks.
+        </p>
+        
+      </div>
+    );
+}
+ 
+export default About;
